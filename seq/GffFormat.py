61f64b7b4319def46cff394b1a6f5a8b3460fcda
xianer <63194738+Daaaaxianer@users.noreply.github.com>
2024-05-21 13:18:22 +0800

    update '--filtergff'

--- seq/GffFormat.py
+++ seq/GffFormat.py
@@ -7,6 +7,8 @@ import pandas as pd
 import argparse
 import numpy as np
 import re
+import os
+import shutil
 # from SeqFormat import readId
 
 ## 设置pandas显示宽度，不省略中间的列
@@ -127,6 +129,55 @@ def readId(idfile):
     print("\nThe id file has been read!\n")
     return linelist
 
+def readGffFilter(gffFilter):
+    '''
+    :param gffFilter: gff filter file :Chr  Start   End Name
+    :return: gffFilter_df
+    '''
+    gff_filter_df = pd.read_csv(gffFilter, sep='\t', header=None, names=["Chr", "Start", "End", "Name"])
+    
+    print(gff_filter_df)
+    return gff_filter_df
+
+def process_gff_data(gff_df, gff_filter_df):
+    output_directory = "output_ids"
+    
+    # 删除已存在的输出目录并重新创建
+    if os.path.exists(output_directory):
+        shutil.rmtree(output_directory)
+    
+    os.makedirs(output_directory)
+
+    # 按照条件筛选出符合要求的 Id
+    selected_ids = []
+    for _, row_filter in gff_filter_df.iterrows():
+        ids = gff_df[(gff_df['chrId'] == row_filter['Chr']) & (gff_df['start'] >= row_filter['Start']) & (gff_df['end'] <= row_filter['End'])]['Id'].tolist()
+        selected_ids.append(','.join(ids))
+
+    gff_filter_df['selId'] = selected_ids
+
+    print(gff_filter_df)
+
+    # 将中间df输出到文件
+    gff_filter_df.to_csv("temp.csv", index=False)
+
+    # 按照Name进行分组
+    grouped = gff_filter_df.groupby('Name')
+
+    for name, group in grouped:
+        # 检查 selId 列是否为空，并且是否有非空白字符串
+        if not group['selId'].empty and any(sel_id.strip() for sel_id in group['selId']):
+            all_ids = []
+            # 遍历每个分组，提取并拆分 selId 列中的 IDs
+            for sel_ids in group['selId']:
+                if sel_ids.strip():  # 检查 selId 是否存在任何字符串
+                    all_ids.extend(sel_ids.split(','))
+
+            # 输出每个 Name 分组的 ID 到单独的文本文件中
+            with open(f"{output_directory}/{name}.txt", 'w') as file:
+                file.write("\n".join(all_ids))
+
+
 
 if __name__ == '__main__':
     # 定义命令行解析器对象
@@ -144,10 +195,16 @@ if __name__ == '__main__':
 
     # 功能2: Read id file and extract bed for MG2C from custom gff
     parser.add_argument("--id2bed", action="store_true",
-                        help="extract bed for MG2C from custom gff(need -i -b)")
+                        help="extract bed for MG2C from custom gff(need -i -f -b)")
     parser.add_argument("-i", "--id", type=str,help="Id file to be extracted")
     parser.add_argument("-f", "--gff", type=str, help="custom gff file")
     parser.add_argument("-b", "--bed", type=str, default='out.bed', help="Extracted bed file")
+
+    # 功能3: Read gff filter file and extract id from custom gff
+    parser.add_argument("--filtergff", action="store_true",
+                        help="filter and extract id from custom gff(need -f -ff)")
+    parser.add_argument('-ff', "--fgff", type=str, help="gff filter file")
+
 
 
     # 解析命令行参数
@@ -161,4 +218,9 @@ if __name__ == '__main__':
 
     if args.id2bed:
         id2bed(args.id, args.gff, args.bed)
+
+    if args.gff and args.fgff:
+        gff_df = readGff(args.gff)
+        gff_filter_df = readGffFilter(args.fgff)
+        process_gff_data(gff_df, gff_filter_df)
 
